130c00db5baa4a44affb38d72ab21a1964fb9d85
Gwendolyn Ruiz <gwen_marina@yahoo.com>
2022-08-30 20:59:05 -0700

    Code to pass get one item test

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/api/v1/merchants/:id/items", to: "api/v1/merchant_items#index"
 
+  get "/api/v1/items/:id", to: "api/v1/items#show"
+
   namespace :api do
     namespace :v1 do
       resources :merchants, only: [:index, :show]
