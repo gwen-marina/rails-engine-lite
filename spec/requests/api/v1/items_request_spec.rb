8d0a1b6fd22986fd02bfe2e3517c0acd705f78e4
Gwendolyn Ruiz <gwen_marina@yahoo.com>
2022-08-31 19:56:56 -0700

    Test: can update an item

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -8,7 +8,9 @@ RSpec.describe "Item's API" do
     get '/api/v1/items'
 
     expect(response).to be_successful
-
+    expect(response.status).to eq(200)
+    expect(response.status).to_not eq(400)
+    
     items_response = JSON.parse(response.body, symbolize_names: true)
     items = items_response[:data]
 
@@ -42,6 +44,8 @@ RSpec.describe "Item's API" do
     get "/api/v1/items/#{item.id}"
    
     expect(response).to be_successful
+    expect(response.status).to eq(200)
+    expect(response.status).to_not eq(404)
 
     items_response = JSON.parse(response.body, symbolize_names: true)
     item = items_response[:data]
@@ -72,15 +76,39 @@ RSpec.describe "Item's API" do
 
     headers = {"CONTENT_TYPE" => "application/json"}
 
-  
     post "/api/v1/items", headers: headers, params: JSON.generate(item: item_params)
     created_item = Item.last
 
     expect(response).to be_successful
+    expect(response.status).to eq(201)
+    expect(response.status).to_not eq(204)
     expect(created_item.name).to eq(item_params[:name])
     expect(created_item.description).to eq(item_params[:description])
     expect(created_item.unit_price).to eq(item_params[:unit_price])
     expect(created_item.merchant_id).to eq(item_params[:merchant_id])
+  end
+
+  it "can edit an existing item" do
+    merchant = create(:merchant)
+    item = create(:item, merchant_id: merchant.id)
+
+    previous_name = Item.name
+    item_params = ({
+                  name: 'balloon',
+                  description: 'this is a balloon',
+                  unit_price: 2.22,
+                  merchant_id: merchant.id
+                })
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    patch "/api/v1/items/#{item.id}", headers: headers, params: JSON.generate({item: item_params})
+    updated_item = Item.find(item.id)
+
+    expect(response).to be_successful
+    expect(response.status).to eq(200)
+    expect(response.status).to_not eq(404)
+    expect(updated_item.name).to_not eq(previous_name)
+    expect(updated_item.name).to eq('balloon')
   end
 
   it "can delete an item" do 
@@ -91,7 +119,32 @@ RSpec.describe "Item's API" do
 
     expect(response).to be_successful
     expect(response.status).to eq(204)
+    expect(response.status).to_not eq(201)
     expect(Item.count).to be(0)
     expect(Item.exists?(item.id)).to be false 
+  end
+
+  it "can get the merchant data for a given item id" do 
+    merchant = create(:merchant)
+    item = create(:item, merchant_id: merchant.id)
+
+    get "/api/v1/items/#{item.id}/merchant"
+  
+    expect(response).to be_successful
+    expect(response.status).to eq(200)
+    expect(response.status).to_not eq(404)
+    expect(merchant.name).to be(merchant.name)
+
+    merchant_response = JSON.parse(response.body, symbolize_names: true)
+    return_merchant = merchant_response[:data]
+
+    expect(return_merchant).to have_key(:id)
+    expect(return_merchant[:id]).to be_a String
+
+    expect(return_merchant).to have_key(:type)
+    expect(return_merchant[:type]).to eq("merchant")
+
+    expect(return_merchant[:attributes]).to have_key(:name)
+    expect(return_merchant[:attributes][:name]).to be_a String
   end
 end
