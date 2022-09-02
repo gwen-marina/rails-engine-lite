e09c610ce68541781e9908a1bd8cadc6d1f0e363
Gwendolyn Ruiz <gwen_marina@yahoo.com>
2022-09-01 22:15:19 -0700

    Feat/code to pass item search test

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -9,14 +9,23 @@ class Api::V1::ItemsController < ApplicationController
   end
 
   def create
-    item = Item.create(item_params) 
-    render json: ItemSerializer.single_item(item), status: :created
+    item = Item.new(item_params) 
+    if item.save
+      render json: ItemSerializer.single_item(item), status: :created
+    else
+      render status: 400
+    end
   end
 
   def update
-    item = Item.update(params[:id], item_params)
-    if item.save
-      render json: ItemSerializer.single_item(item)
+    if Item.exists?(params[:id])
+      item = Item.find(params[:id])
+      item.update(item_params)
+      if item.save
+        render json: ItemSerializer.single_item(item)
+      else
+        render status: 404
+      end
     else
       render status: 404
     end
@@ -26,6 +35,12 @@ class Api::V1::ItemsController < ApplicationController
     item = Item.find(params[:id])
     render json: Item.destroy(params[:id]), status: 204
   end
+
+  def find_all
+    item = Item.find_all_by_name(params[:name])
+    render json: ItemSerializer.format_items(item)
+  end
+
 
   private
 
